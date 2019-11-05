1e8d0d24a37ae2335cea097c74cdb80871c22e3b
EJ <ejg132@gmail.com>
2019-11-05 12:07:40 -0800

    Recursion Big O notation

---
+++ Recursive_BigO.js
@@ -0,0 +1,61 @@
+//Question: #1
+
+//Answer: O(n)
+//Explanation: Sheep depends on the amount the user puts
+
+
+//Question: #2 
+
+//Answer: O(n)
+//Eplanation: Due to the exponent we have to repeat the function
+
+
+//Question #3
+
+//Answer: O(n)
+//Explanation: Dependent on length of the word
+
+//Question #4
+
+//Answer: O(n)
+//Explanation: Dependent on the number inputed for the triangle
+
+//Question #5
+
+//Answer: O(n)
+//Explanation: Dependent on the seperator
+
+//Question #6
+
+//Answer: O(N)
+//Explanation: Dependent on the size of the number
+
+//Question #7
+
+//Answer: O(n)
+//Explanation: Dependent on the size of the number
+
+// Question #8
+
+// Answer: O(log(n))
+// Explanation: Depends on if it is L R D U cases and it has to iterate through each case
+
+// Question #9
+
+// Answer: Same as #8
+// Explanation: Same as #8
+
+// Question #10
+
+// Answer: O(n^k)
+// Explanation: Since the recursion is called within the for loop there are nested for loops being called
+
+// Question #11
+
+// Answer: SKIP
+// Explanation: SKIP
+
+// Question #12
+
+// Answer: O(n)
+// Explanation: Number of times iterated depends on how large the number is
