510143d5341608140e18b0a5e516b465dab167ac
Marlon Agno <mdj.agno@gmail.com>
2019-11-05 20:13:11 +0000

    added Big O time complexity for iterative solutions

---
+++ Iterative_BigO.js
@@ -0,0 +1,21 @@
+// Question 1)
+// Answer: O(n), number of iterations depends linearly on input size
+
+// Question 2)
+// Answer: O(n), number of iterations depends linearly on input size
+
+// Question 3)
+// Answer: O(n), number of iterations depends on string length
+
+// Question 4)
+// Answer: O(n), number of iterations depends on number
+
+// Question 5)
+// Answer: O(n) number of iterations depends on string length
+
+// Question 6)
+// Answer: O(n) as number of iterations depends on number selected from series
+
+// Question 7)
+// Answer: O(n)
+
